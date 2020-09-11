b6761f09cde70a451a13f44cd63b4818a03f0b26
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-11 10:52:48 +0200

    three new child classes of the Dog class

--- exercise/oop/dog.py
+++ exercise/oop/dog.py
@@ -1,11 +1,25 @@
 class Dog:
+    species = "Canis familiaris"
+
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
 
-    # Instance method
-    def description(self):
+    # Replace .description() with __str__()
+    def __str__(self):
         return f"{self.name} is {self.age} years old"
 
     def speak(self, sound):
         return f"{self.name} says {sound}"
+
+
+class JackRussellTerrier(Dog):
+    pass
+
+
+class Dachshund(Dog):
+    pass
+
+
+class Bulldog(Dog):
+    pass
