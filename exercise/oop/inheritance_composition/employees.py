2651b600a000f600257e3dd81ba6ceefa64244b4
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-12 09:54:30 +0200

    The Class Explosion Problem

--- exercise/oop/inheritance_composition/employees.py
+++ exercise/oop/inheritance_composition/employees.py
@@ -31,3 +31,23 @@ class CommissionEmployee(SalaryEmployee):
     def calculate_payroll(self):
         fixed = super().calculate_payroll()
         return fixed + self.comission
+
+
+class Manager(SalaryEmployee):
+    def work(self, hours):
+        print(f"{self.name} screams and yells for {hours} hours.")
+
+
+class Secretary(SalaryEmployee):
+    def work(self, hours):
+        print(f"{self.name} expends {hours} hours doing office paperwork.")
+
+
+class SalesPerson(CommissionEmployee):
+    def work(self, hours):
+        print(f"{self.name} expends {hours} hours on the phone.")
+
+
+class FactoryWorker(HourlyEmployee):
+    def work(self, hours):
+        print(f"{self.name} manufactures gadgets for {hours} hours.")
