24beb8778f51f416f2691aca109c7fd0cc7de45f
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-12 10:09:19 +0200

    method resolution order (MRO) 1

--- exercise/oop/inheritance_composition/employees.py
+++ exercise/oop/inheritance_composition/employees.py
@@ -51,3 +51,7 @@ class SalesPerson(CommissionEmployee):
 class FactoryWorker(HourlyEmployee):
     def work(self, hours):
         print(f"{self.name} manufactures gadgets for {hours} hours.")
+
+
+class TemporarySecretary(Secretary, HourlyEmployee):
+    pass
